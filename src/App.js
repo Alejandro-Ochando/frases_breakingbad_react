27aff45a8866122a4d0bfd890461072d12e555c0
Alejandro-Ochando <alejandro.ochando@live.com>
2020-10-01 18:23:22 +0200

    Estilos y creando la consulta a la API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
+const Contenedor = styled.div`
+  display:flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction:column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient( top left, #007d35 0%, #007d35 40%, #0f574e 100%  );
+  background-size:305px;
+  font-family: 'Arial', Arial, Helvetica, sans-serif;
+  color: #FFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+
+  `;
 
 function App() {
+
+  const consultarApi = async () => {
+    const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    console.log(frase[0]);
+   
+  }
+ 
+
   return (
-    <h1>Frases</h1>
+
+    <Contenedor>
+      <Boton
+        onClick={consultarApi}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
+    
   );
 }
 
